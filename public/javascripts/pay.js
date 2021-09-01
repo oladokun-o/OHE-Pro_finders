22936a4ea2929321ee45b80bfde816ea1be375fa
Oladokun <oladipupooladokun@gmail.com>
2021-09-01 16:09:56 -0700

    Update pay.js

--- public/javascripts/pay.js
+++ public/javascripts/pay.js
@@ -10,29 +10,30 @@ $('.button').click(function (e) {
     $.ajax({
         method: "POST",
         contentType: "application/json; charset=utf-8",
-        url: "https://api.flutterwave.com/v3/payments",
-        headers: {
-            Authorization: "Bearer FLWSECK_TEST-8c3bd46a6862a54dd7cceb3aa16c236e-X",
-        },
+        url: 'https://api.flutterwave.com/v3/payments',
+        headers: {"Authorization": "Bearer FLWSECK_TEST-8c3bd46a6862a54dd7cceb3aa16c236e-X",},
         data: {
-            tx_ref: userId,
-            amount: $('.amount').text(),
-            redirect_url: "localhost:8800/dashboard",
-            payment_options: "card",
-            meta:{
-               consumer_id: userId,
+            "tx_ref":"hooli-tx-1920bbtytty",
+            "amount":"100",
+            "currency":"NGN",
+            "redirect_url":"https://webhook.site/9d0b00ba-9a69-44fa-a43d-a82c33c36fdc",
+            "payment_options":"card",
+            "meta":{
+               "consumer_id":23,
+               "consumer_mac":"92a3-912ba-1192a"
             },
-            customer:{
-               email: email,
-               phonenumber: phone,
-               name: firstname + ' ' + lastname
+            "customer":{
+               "email":"user@gmail.com",
+               "phonenumber":"080****4528",
+               "name":"Yemi Desola"
             },
-            customizations:{
-               title: "Oose Heritage Enterprise Payments",
-               description: "5 minutes chat session with " + expert,
-               logo: "http://main-ohe-pro-finders.herokuapp.com/images/OHE-Logo.png"
+            "customizations":{
+               "title":"Pied Piper Payments",
+               "description":"Middleout isn't free. Pay the price",
+               "logo":"https://assets.piedpiper.com/logo.png"
             }
-         },
+         }
+         ,
          success:(response)=>{
              console.log('success:'+response)
          },
